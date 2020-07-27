ce5df7fd9dc2410fe599d95878750e9f963ceceb
Sakhaout <convags@jmfamily.com>
2020-07-27 00:06:28 -0400

    Code 1st Push to FirstDraft Branch

--- src/main/java/sample/basepage/BasePage.java
+++ src/main/java/sample/basepage/BasePage.java
@@ -31,7 +31,7 @@ public class BasePage {
 	private static String chromeDriver = "\\src\\main\\resources\\Driver\\chromedriver.exe";
 	private static String geckoDriver = "\\src\\main\\resources\\Driver\\geckodriver.exe";
 	private static String ieDriver = "\\src\\main\\resources\\Driver\\IEDriverServer.exe";
-	
+
 	public BasePage() {
 		try {
 			String properties_path = System.getProperty("user.dir") + "\\src\\main\\resources\\PropertiesFile\\config.properties";
@@ -43,29 +43,29 @@ public class BasePage {
 			ex.getStackTrace();
 		}
 	}
-	
+
 	public static void initialition(String browser) {
 		//String browser = properties_obj.getProperty("browser"); //Get properties from properties file.
-//		String path_chromeDriver = System.getProperty("user.dir") + chromeDriver;
-//		String path_gekoDriiver = System.getProperty("user.dir") + geckoDriver;
-//		String path_IEDriver = System.getProperty("user.dir") + ieDriver;
+		//		String path_chromeDriver = System.getProperty("user.dir") + chromeDriver;
+		//		String path_gekoDriiver = System.getProperty("user.dir") + geckoDriver;
+		//		String path_IEDriver = System.getProperty("user.dir") + ieDriver;
 		log.info("  Launching Browser - "+browser);
 		if(browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			DesiredCapabilities cap = new DesiredCapabilities();
 			cap.setCapability("browserName", "chrome");
-		    ChromeOptions chromeOptions = new ChromeOptions();
-		    chromeOptions.addArguments("--verbose");
-		    chromeOptions.addArguments("--whitelisted-ips=''");
-		    chromeOptions.merge(cap);
-			try {
-				driver = new RemoteWebDriver(new URL("http://52.91.121.94:8080/"),chromeOptions);
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-//			System.setProperty("webdriver.chrome.driver", path_chromeDriver);
-			//driver = new ChromeDriver(chromeOptions);
+			ChromeOptions chromeOptions = new ChromeOptions();
+//			chromeOptions.addArguments("--verbose");
+//			chromeOptions.addArguments("--whitelisted-ips=''");
+			chromeOptions.merge(cap);
+			//			try {
+			//				driver = new RemoteWebDriver(new URL("http://52.91.121.94:8080/"),chromeOptions);
+			//			} catch (MalformedURLException e) {
+			//				// TODO Auto-generated catch block
+			//				e.printStackTrace();
+			//			}
+			//			System.setProperty("webdriver.chrome.driver", path_chromeDriver);
+			driver = new ChromeDriver();
 		}else if(browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			DesiredCapabilities cap = new DesiredCapabilities();
@@ -76,35 +76,35 @@ public class BasePage {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-//			System.setProperty("webdriver.gecko.driver", path_gekoDriiver);
-//			driver = new FirefoxDriver();
+			//			System.setProperty("webdriver.gecko.driver", path_gekoDriiver);
+			//			driver = new FirefoxDriver();
 		}else if(browser.equalsIgnoreCase("IE")) {
 			// Set desired capabilities to Ignore IEDriver IGNORING_SECURITY_DOMAINS.
-//			DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-//			capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-			
+			//			DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+			//			capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+
 			// Set desired capabilities to Ignore IEDriver zoom level settings and disable native events.
-//			capabilities.setCapability("EnableNativeEvents", false);
-//			capabilities.setCapability("ignoreZoomSetting", true);
-		//	capabilities.setCapability("browserName", "IE");
+			//			capabilities.setCapability("EnableNativeEvents", false);
+			//			capabilities.setCapability("ignoreZoomSetting", true);
+			//	capabilities.setCapability("browserName", "IE");
 			WebDriverManager.iedriver().setup();
-//			try {
-//				driver = new RemoteWebDriver(new URL(" "),capabilities);
-//			} catch (MalformedURLException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			System.setProperty("webdriver.ie.driver", path_IEDriver);
+			//			try {
+			//				driver = new RemoteWebDriver(new URL(" "),capabilities);
+			//			} catch (MalformedURLException e) {
+			//				// TODO Auto-generated catch block
+			//				e.printStackTrace();
+			//			}
+			//			System.setProperty("webdriver.ie.driver", path_IEDriver);
 			driver = new InternetExplorerDriver();
 		}
-		
+
 		driver.manage().window().maximize();  //Windows will maximize.
 		driver.manage().deleteAllCookies();		//Delete all cookies.
 		CommonFunction.pageLoadTimeout(Page_Load_TimeOut); //Wait untile page are fully loeaded.
 		CommonFunction.implicitlyWait(Implicitly_Wait); //Wait until element is found, but wait until a time.
 		log.info("  Application is opening \n");
 		driver.get(properties_obj.getProperty("PCRS_URI")); //Lunch the url.		
-		
+
 	}
 
 
